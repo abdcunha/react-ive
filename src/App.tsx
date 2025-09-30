89e2c670d2aebc936111c78cb63c1feecbeebfa1
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-09-30 23:58:36 +0530

    BugFix: added missing property value of the type object

--- src/App.tsx
+++ src/App.tsx
@@ -2,7 +2,7 @@ import { useState, useEffect } from 'react'
 import './App.css'
 
 function App() {
-  const [health, sethHealth] = useState<{status?: string, }>({})
+  const [health, sethHealth] = useState<{ status?: string, message?: string }>({})
   const [error, setError] = useState<string>('')
 
   useEffect(() => {
@@ -23,7 +23,18 @@ function App() {
 
   return (
     <>
-      
+      {/* Health Status Display */}
+      <div className="card">
+        <h2>Backend Health Status</h2>
+        {error ? (
+          <p style={{ color: "red" }}>{error}</p>
+        ) : (
+          <div>
+            <p>Status: {health.status || "Loading..."}</p>
+            <p>Message: {health.message || "Loading..."}</p>
+          </div>
+        )}
+      </div>
     </>
   )
 }
