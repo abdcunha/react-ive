032e0edd3206eb5286656155c2fbd1afccd9f7ef
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-10-30 23:09:29 +0530

    Dummy commit

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,5 +1,7 @@
 import type { HealthResponse } from "../types/HealthResponse";
 
+
+// Base API
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const healthApi = {
