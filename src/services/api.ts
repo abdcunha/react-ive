de1d3c331801a5089a7fbf5942168bd4cfdeb5ea
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-11-02 23:07:31 +0530

    Removed the dummy comment

--- src/services/api.ts
+++ src/services/api.ts
@@ -1,7 +1,5 @@
 import type { HealthResponse } from "../types/HealthResponse";
 
-
-// Base API
 const API_BASE_URL = import.meta.env.VITE_API_URL || "http://localhost:3000";
 
 export const healthApi = {
