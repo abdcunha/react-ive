558ed7891ab8fa0fd1a98da5b48013341f8a6b71
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-13 23:18:55 +0530

    Added window width comp

---
+++ src/components/windowWidth.tsx
@@ -0,0 +1,9 @@
+import React from "react";
+import { useWindowWidth } from "../hooks/useWindowWidth";
+
+const WindowWidth: React.FC = () => {
+    const width = useWindowWidth();
+    return <div>Window width: {width}px</div>
+};
+
+export default WindowWidth;
