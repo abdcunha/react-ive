72c8aeb0aba0d9045b6d621b69cb2b6c41cccd24
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-14 15:11:08 +0530

    component usage update in app component

--- src/App.tsx
+++ src/App.tsx
@@ -1,18 +1,34 @@
 import "./App.css";
 import { HealthStatus } from "./components/HealthStatus";
 import { useHealth } from "./hooks/useHealth";
+import Greeting from "./components/Greeting";
+import Counter from "./components/Counter";
+import Timer from "./components/Timer";
+import { ThemeProvider } from "./context/ThemeContext";
+import ThemeSwitcher from "./components/ThemeSwitcher";
+import ExpensiveCalculation from "./components/ExpensiveCalculation";
+import WindowWidth from "./components/WindowWidth";
 
 function App() {
   const { health, error, loading } = useHealth();
 
   return (
-    <div className="app">
-      <HealthStatus
-        health={health}
-        error={error}
-        loading={loading}
-      />
-    </div>
+    <ThemeProvider>
+      <div>
+        <Greeting name="World!" />
+        <Counter />
+        <Timer />
+        <ThemeSwitcher />
+        <ExpensiveCalculation />
+        <WindowWidth />
+        <HealthStatus
+          health={health}
+          error={error}
+          loading={loading}
+        />
+      </div>
+    </ThemeProvider>
+
   );
 };
 
