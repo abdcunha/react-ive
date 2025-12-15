320e98090c6c14e248cba589de8708eee39e5db2
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-15 21:55:23 +0530

    theme switching

--- src/App.tsx
+++ src/App.tsx
@@ -4,32 +4,39 @@ import { useHealth } from "./hooks/useHealth";
 import Greeting from "./components/Greeting";
 import Counter from "./components/Counter";
 import Timer from "./components/Timer";
-import { ThemeProvider } from "./context/ThemeContext";
+import { ThemeProvider, useTheme } from "./context/ThemeContext";
 import ThemeSwitcher from "./components/ThemeSwitcher";
 import ExpensiveCalculation from "./components/ExpensiveCalculation";
 import WindowWidth from "./components/WindowWidth";
 
-function App() {
+const ThemedApp: React.FC = () => {
+  const { theme } = useTheme();
   const { health, error, loading } = useHealth();
 
   return (
-    <ThemeProvider>
-      <div>
-        <Greeting name="World!" />
-        <Counter />
-        <Timer />
-        <ThemeSwitcher />
-        <ExpensiveCalculation />
-        <WindowWidth />
-        <HealthStatus
-          health={health}
-          error={error}
-          loading={loading}
-        />
-      </div>
-    </ThemeProvider>
 
+    <div className={theme === "dark" ? "dark-theme" : "light-theme"}>
+      <Greeting name="World!" />
+      <Counter />
+      <Timer />
+      <ThemeSwitcher />
+      <ExpensiveCalculation />
+      <WindowWidth />
+      <HealthStatus
+        health={health}
+        error={error}
+        loading={loading}
+      />
+    </div>
   );
 };
+
+const App: React.FC = () => {
+  return (
+    <ThemeProvider>
+      <ThemedApp />
+    </ThemeProvider>
+  );
+}
 
 export default App;
