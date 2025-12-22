de1783f4bebce4abf6d0cf4d3013affeba6cff06
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-22 14:53:58 +0530

    mock rest-over-http API call in test

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,7 +1,23 @@
 import { render, screen } from "@testing-library/react";
 import App from "./App";
+import { vitest as vi } from "vitest"
+
+beforeAll(() => {
+    globalThis.fetch = vi.fn().mockResolvedValue({
+        ok: true,
+        json: async () => ({
+            status: "ok",
+            timestamp: new Date().toISOString(),
+            uptime: 123.45
+        })
+    });
+});
 
 test("renders ThemeSwitcher", () => {
     render(<App />);
     expect(screen.getByRole("button", { name: /theme/i })).toBeInTheDocument();
+});
+
+afterAll(() => {
+    vi.resetAllMocks();
 });
