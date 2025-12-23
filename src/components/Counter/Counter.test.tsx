5b27b54708dd24e3182c9cf0160569117a9633c0
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-23 23:24:43 +0530

    stying and test update

--- src/components/Counter/Counter.test.tsx
+++ src/components/Counter/Counter.test.tsx
@@ -3,10 +3,14 @@ import Counter from "./Counter";
 
 test("Counter increments and decrements", () => {
     render(<Counter />);
-    const incBtn = screen.getByRole("button", { name: /add/i });
+    const incBtn = screen.getByRole("button", { name: "+" });
+    const decBtn = screen.getByRole("button", { name: "-" });
     const value = screen.getByRole("paragraph");
     
     expect(value).toHaveTextContent("0");
     fireEvent.click(incBtn);
     expect(value).toHaveTextContent("1");
+    fireEvent.click(decBtn);
+    expect(value).toHaveTextContent("0");
+
 });
