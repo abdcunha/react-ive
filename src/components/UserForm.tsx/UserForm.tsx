27b06849b451538a1165b4550a99c7dc59443919
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-26 16:27:56 +0530

    user form scaffolding

---
+++ src/components/UserForm.tsx/UserForm.tsx
@@ -0,0 +1,14 @@
+import React, { useState } from "react";
+
+const UserForm: React.FC = () => {
+    const [name, setName] = useState("");
+    const [submitted, setSubmitted] = useState("");
+
+    return (
+        <div>
+            
+        </div>
+    );
+}
+
+export default UserForm;
