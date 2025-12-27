64aed64a3459e365035366ded6ffc832fb0ce50c
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-27 16:07:52 +0530

    random seconds

--- src/components/Timer/Timer.tsx
+++ src/components/Timer/Timer.tsx
@@ -3,8 +3,10 @@ import React, { useEffect, useState } from "react";
 const Timer: React.FC = () => {
     const [seconds, setSeconds] = useState(0);
 
+    // space bar to pause the number
+    // and if the number hits a logic then there should be confetti shower
     useEffect(() => {
-        const interval = setInterval(() => setSeconds((s) => s + 1), 1000);
+        const interval = setInterval(() => setSeconds(Math.floor(Math.random() * (10000 - 0 + 1)) + 0), 250);
         return () => clearInterval(interval);
     }, []);
 
