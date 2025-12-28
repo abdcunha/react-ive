001347766a6b8ccada6c08a67571e568cad7a93e
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-28 15:32:58 +0530

    fixed test warning

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -1,6 +1,7 @@
 import { render } from "@testing-library/react";
 import App from "./App";
 import { vitest as vi } from "vitest"
+import { act } from "react";
 
 beforeAll(() => {
     globalThis.fetch = vi.fn().mockResolvedValue({
@@ -13,8 +14,14 @@ beforeAll(() => {
     });
 });
 
-test("renders ThemeSwitcher", () => {
-    render(<App />);
+test("renders ThemeSwitcher", async () => {
+    let container: HTMLElement;
+
+    await act(async () => {
+        const rendered = render(<App />);
+        container = rendered.container;
+    });
+    expect(container!.querySelector('.light-theme')).toBeInTheDocument();
 });
 
 afterAll(() => {
