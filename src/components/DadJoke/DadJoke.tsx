4d834140d68598eb8a272cb862c48c09d77261ec
Anoop Beven Dcunha <anoopbevendcunha@gmail.com>
2025-12-31 21:20:41 +0530

    dj comp update

--- src/components/DadJoke/DadJoke.tsx
+++ src/components/DadJoke/DadJoke.tsx
@@ -1,9 +1,18 @@
 import React, { useState } from "react";
 
 const DadJoke: React.FC = () => {
+    const [joke, setJoke] = useState<string>("");
+
+    const fetchJoke = async () => {
+        const res = await fetch("https://icanhazdadjoke.com/");
+        const data = await res.json();
+        setJoke(data.joke);
+    }
+
     return (
         <>
-            
+            <button onClick={fetchJoke}>Hye, Dad</button>
+            <p>{joke}</p>
         </>
     )
 };
